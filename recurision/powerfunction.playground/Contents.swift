5fc429b763162eaf052516e172619f8357242cab
Sameera Leola <sameeraleola@gmail.com>
2020-07-23 06:16:24 -0700

    Practicing recursion with power function

---
+++ recurision/powerfunction.playground/Contents.swift
@@ -0,0 +1,23 @@
+import UIKit
+
+/*
+ Using recursion to implement a power function.  Af power function is one of the form : y = x^n
+ 
+ In a power function n is any real number constant.  Real numbers are all the irrational numbers such as
+ the integer −5 and the fraction 4/3, and all the irrational numbers, such as √2.  Pi ( π ) is also a real number.
+ 
+  Example of power functions: 2^3 = 2 * 2 * 2 = 8
+ */
+
+func power(number : Int,  exponent : Int) -> Int {
+    
+    // Base case
+    if exponent == 0 {
+        return 1
+    } else {                                    // Recursive case
+        print("\(number) * \(exponent - 1) = \(number * exponent - 1)")
+        return number * power(number: number, exponent: exponent-1)
+    }
+}
+
+print(power(number: 2, exponent: 3))
