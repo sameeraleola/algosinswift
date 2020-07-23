23cad4793761d168cc2aa550e6e14bf9b5b60c71
Sameera Leola <sameeraleola@gmail.com>
2020-07-23 06:27:13 -0700

    Completed power function with recursion

--- recurision/powerfunction.playground/Contents.swift
+++ recurision/powerfunction.playground/Contents.swift
@@ -9,15 +9,26 @@ import UIKit
   Example of power functions: 2^3 = 2 * 2 * 2 = 8
  */
 
+// This function requires passing in the number that will be multiplied (number)
+// and the exponent which specifies how many times to multiple number (exponent)
 func power(number : Int,  exponent : Int) -> Int {
     
     // Base case
     if exponent == 0 {
         return 1
-    } else {                                    // Recursive case
+    } else {
+        // Recursive case                       Moving toward base case
         print("\(number) * \(exponent - 1) = \(number * exponent - 1)")
         return number * power(number: number, exponent: exponent-1)
     }
 }
 
 print(power(number: 2, exponent: 3))
+
+/*
+ What is this function doing:
+ STACK
+ Push: 2 * power(2, 2)  = 2*2 = 4
+ Push: 2 * power(2, 1) = 4*2 = 8
+ Push: 2 * power(2, 0) = 0
+ */
