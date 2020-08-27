414177aabf9a07cca1b08b919bb56dbdaf2bd549
Sameera Leola <sameeraleola@gmail.com>
2020-08-26 23:14:19 -0500

    Begin traversal of Binary Tree

--- binarytrees/binarytrees.playground/Pages/In order traversal.xcplaygroundpage/Contents.swift
+++ binarytrees/binarytrees.playground/Pages/In order traversal.xcplaygroundpage/Contents.swift
@@ -2,6 +2,33 @@
 
 import Foundation
 
+/*
+  ******** BINARY TREE IN ORDER TRAVERSAL ********
+ In order traversal of a binary tree is a depth first traversal.
+ There are three DFT orders:
+  • Inorder : Left -> Root -> Right
+  • Preorder : Root -> Left -> Right
+  • Postorder : Left -> Right -> Root
+ 
+ Example binary tree used in this explaination:
+                     1
+                  /    \
+                 2      3
+                / \
+               4   5
+ 
+• Inorder Example: (Left -> Root -> Right) = 4 -> 2 -> 5 -> Root -> 1
+• Prerder Example: (Root -> Left -> Right) = Root -> 2 -> 4 -> 5 -> 3
+• Postorder Example: (Root -> Left -> Right) = 4 -> 5 -> 2 -> 3 -> Root
+ 
+ • Inorder : Begins at the left most leaf node and goes up.
+ • Prerder : Begins at the root and visis each node in order: root -> left child on the righ+ ->
+ • Postorder Example: (Root -> Left -> Right) = 4 -> 5 -> 2 -> 3 -> Root
+ 
+ */
+
+//  ******** BINARY TREE DATA STRUCTURE ********
+
 
 
 //: [Next](@next)
--- binarytrees/binarytrees.playground/Pages/binarytree.xcplaygroundpage/Contents.swift
+++ binarytrees/binarytrees.playground/Pages/binarytree.xcplaygroundpage/Contents.swift
@@ -52,5 +52,4 @@ two.rightChild = six
 
 
 
-
 // ******** IMPLEMENT A BINARY TREE ********
